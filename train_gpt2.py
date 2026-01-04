bbca6d9a90b574d3db0c830767029b61072cd737
Makarez <makarezp@gmail.com>
2026-01-04 19:25:11 +0100

    implement MLP module

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
 @dataclass
 class GPTConfig:
     block_size: int = 256
@@ -11,6 +10,20 @@ class GPTConfig:
     n_layer: int = 6
     n_head: int = 6
     n_embd: int = 384
+
+class MLP(nn.Module):
+    
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 class Block(nn.Module):
     def __init__(self, config: GPTConfig):
