fc5f99bf948cee92455a094b306966be5225a887
Makarez <makarezp@gmail.com>
2026-06-27 22:02:59 +0200

    clean up

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 import time
 
 
@@ -220,34 +221,61 @@ train_loader = DataLoaderLite(B=4, T=1024)
 
 
 model = GPT(GPTConfig())
-model = torch.compile(model)
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+
+def get_lr(it):
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    if it > max_steps:
+        return min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+
 model.to(device)
 
+grad_accum_steps = 8
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+use_fused = True
+print(f"using fused AdamW: {use_fused}")
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, fused=use_fused)
 for i in range(50):
-    if device == "mps":
-        torch.mps.synchronize()
-    elif device == "cuda":
-        torch.cuda.synchronize()
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
+
+    # determine and set the learning rate for this iteration
+    lr = get_lr(i)
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
     optimizer.zero_grad()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
-    loss.backward()
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     optimizer.step()
+
     if device == "mps":
         torch.mps.synchronize()
     elif device == "cuda":
         torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0) * 1000  # milliseconds
-    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
+    tokens_per_sec = tokens_processed / (t1 - t0)
     print(
-        f"step {i}, loss: {loss.item():.6f}, time: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}"
+        f"step {i}, loss: {loss_accum.item():.6f}, lr: {lr:.4e}, time: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}"
     )
 
 import tiktoken
